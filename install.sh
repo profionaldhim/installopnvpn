d41c78bd8c24ff6c206726ef26e08568f4524d37
profionaldhim <35074237+profionaldhim@users.noreply.github.com>
2019-12-23 18:50:56 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+figlet "install opnvpn "
+apt-get update
+
+apt-get install network-manager-pptp network-manager-pptp-gnome
+
+
+apt-get install network-manager-pptp
+apt-get install network-manager-pptp-gnome
+
+
+sudo apt-get install network-manager-openvpn
+sudo apt-get install network-manager-openvpn-gnome
+sudo apt-get install network-manager-strongswan
+sudo apt-get install network-manager-vpnc
+sudo apt-get install network-manager-vpnc-gnome
